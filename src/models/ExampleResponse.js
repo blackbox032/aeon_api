9291da10b48d8ef936449fce6eea73918dd051a6
Farren <farrenh@gmail.com>
2020-03-28 14:31:55 +0200

    modified response class example logic

--- src/models/ExampleResponse.js
+++ src/models/ExampleResponse.js
@@ -1,11 +1,21 @@
-export default class ExampleRequest {
+export default class ExampleResponse extends ResponseWrapper {
   constructor(xml) {
-    // Need code to convert from response xml supplied to
+    //this statement calls the constructor of the parent ResponseWrapper class,
+    //which produces the properties
+    // this.sessionId
+    // this.eventType
+    // this.event
+    // this.event.eventCode
+    // this.event.eventDescription
+    // this.innerXML
+    super(xml);
+
+    // Need code to convert this.innerXML (from the above call to the parent ResponseWrapper constructor)
     // json properties, resulting in
     //
-    // this.property1
-    // this.property2
-    // this.property3
+    // this.response.property1
+    // this.response.property2
+    // this.response.property3
     // ...
   }
 }
---
+++ src/models/ResponseWrapper.js
@@ -0,0 +1,13 @@
+export default class ResponseWrapper {
+  constructor(xml) {
+    //Need code to convert from response xml to
+    //json properties, resulting in the following properties
+    //
+    // this.sessionId
+    // this.eventType
+    // this.event
+    // this.event.eventCode
+    // this.event.eventDescription
+    // this.innerXML
+  }
+}
