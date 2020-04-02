1119c89eb3779cf220124a8cafad284d43fe9018
Johan Van Zyl <johanvanzyl@Johans-MacBook-Pro-2.local>
2020-04-02 15:10:22 +0200

    Electricity Client update

--- src/clients/electricityClient.js
+++ src/clients/electricityClient.js
@@ -1,28 +1,38 @@
 const socketRequest = require("./socketRequest");
-const airtimeTopUpAdapter = require("../adapters/airtimeTopUpAdapter");
+const meterTopUpAdapter = require("../adapters/meterVoucherAdapter");
+const meterConfirmAdapter = require("../adapters/meterConfirmAdapter");
 
-const port = process.env.PORT || 7898;
-const host = process.env.EXTERNAL_URL || "196.38.158.118";
+const port = process.env.PORT || 7893;
+const host = process.env.EXTERNAL_URL || "196.26.170.3";
 const ttl = process.env.TTL || 60000;
-const userPin = process.env.PIN || 011234;
-const deviceId = process.env.DEVICE_ID || 2215;
+const userPin = process.env.PIN || "011234";
+const deviceId = process.env.DEVICE_ID || "7305";
 const deviceSer = process.env.DEVICE_SER || "TiZZIw779!";
 
-//TODO: GetBundle
-async function doElectricityTopUp(transType, reference, phoneNumber, amount) {
-  xml = airtimeTopUpAdapter.toXML(
-    userPin,
-    deviceId,
-    deviceSer,
-    transType,
-    reference,
-    phoneNumber,
-    amount
+async function doElectricityTopUp(sessionId, confrimRef, reference) {
+  xml = meterTopUpAdapter.toXML(
+    sessionId,
+    confrimRef,
+    reference
   );
   return await socketRequest(host, port, xml, ttl).then(serverResponse => {
     console.log("Electricity Topup response: ", serverResponse);
-    return airtimeTopUpAdapter.toJS(serverResponse);
+    return meterTopUpAdapter.toJS(serverResponse);
   });
 }
 
-module.exports = { doAirtimeTopUp };
+async function doVerifyMeter(meterNumber, amount) {
+        xml = meterConfirmAdapter.toXML(
+          userPin,
+          deviceId,
+          deviceSer,
+          meterNumber,
+          amount
+        );
+        return await socketRequest(host, port, xml, ttl).then(serverResponse => {
+          console.log("Meter verify response: ", serverResponse);
+          return meterConfirmAdapter.toJS(serverResponse);
+        });
+      }
+
+module.exports = { doElectricityTopUp, doVerifyMeter };
