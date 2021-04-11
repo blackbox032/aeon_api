35930b1d865773b0c3110b2b6a37295658af221b
rivtam <tazy.spot@gmail.com>
2021-04-11 18:06:29 +0200

    Create timeout

--- src/clients/dataClient.js
+++ src/clients/dataClient.js
@@ -7,7 +7,7 @@ const mnoDataBundleValidationAdapter = require("../adapters/mnoDataBundleValidat
 
 const port = process.env.AEON_AIRTIME_PORT || 7800;
 const host = process.env.AEON_AIRTIME_URL || "aeon.qa.bltelecoms.net";
-const ttl = process.env.TTL || 60000;
+const ttl = process.env.TTL || 60;
 const userPin = process.env.AEON_AIRTIME_PIN || "016351";
 const deviceId = process.env.AEON_AIRTIME_DEVICE_ID || "865181";
 const deviceSer = process.env.AEON_AIRTIME_DEVICE_SER || "w!22!t";
